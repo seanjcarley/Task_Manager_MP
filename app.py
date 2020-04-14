4e09a98f2ebf1f625518553392ccf3d4935f5b0a
Sean <seanjcarley@outlook.com>
2020-04-14 20:42:00 +0100

    updated app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello():
 
 if __name__ == '__main__':
     app.run(
-        host=os.environ.get('IP', '0.0.0.0'),
-        port=int(os.environ.get('PORT', '8080')),
+        host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
         debug=True
     )
