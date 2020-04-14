313ec362d6a38143462da6c819d59e4fdfa07cc8
Sean <seanjcarley@outlook.com>
2020-04-14 20:54:40 +0100

    added default IP tp app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello():
 
 if __name__ == '__main__':
     app.run(
-        host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
+        host=os.environ.get('IP','0.0.0.0'),
+        port=int(os.environ.get('PORT', 8080)),
         debug=True
     )
