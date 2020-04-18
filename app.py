78d41cbb4f403f35546c3cd4cc72886483786cc9
Sean <seanjcarley@outlook.com>
2020-04-18 15:33:20 +0100

    Updated HTMl files

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo, pymongo
+from bson.objectid import ObjectId
+import env as config
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello world ...again!'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
+
+
+# def hello():
+#     return 'Hello world ...again!'
 
 
 if __name__ == '__main__':
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ["MONGO_URI"] = 'mongodb+srv://root:sJ018316995c@my-first-cluster-3cror.mongodb.net/task_manager?retryWrites=true&w=majority'
